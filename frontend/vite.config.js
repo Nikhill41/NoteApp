01f3e7f66a920a27dd7a5508a14e0555cc4b273b
Nikhil Pal <nikhilpal630622@gmail.com>
2026-04-15 01:11:04 +0530

    server port changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],server: {
+    port: 3000   // 👈 change port here
+  }
 })
